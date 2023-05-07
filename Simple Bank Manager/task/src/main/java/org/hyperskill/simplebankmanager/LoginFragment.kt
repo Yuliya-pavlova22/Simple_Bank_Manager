e5291ee78d807e011a3c107fb14b0dd7c8facab1
lilaacc <eliseeva.ula@gmail.com>
2023-05-07 17:32:30 +0300

    этап 3 3 теста успех

--- Simple Bank Manager/task/src/main/java/org/hyperskill/simplebankmanager/LoginFragment.kt
+++ Simple Bank Manager/task/src/main/java/org/hyperskill/simplebankmanager/LoginFragment.kt
@@ -21,9 +21,8 @@ private const val ARG_PARAM2 = "param2"
  * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-    class LoginFragment() : Fragment() {
-
-
+class LoginFragment() : Fragment() {
+    private val accountService = IoC.AccountService
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,27 +31,29 @@ private const val ARG_PARAM2 = "param2"
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-     override fun onViewCreated(view: View, savedInstanceState: Bundle? ){
-         super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-         val intent = (view.context as AppCompatActivity).intent
-         val username = intent.getStringExtra("username") ?: "Lara"
-         val password = intent.getStringExtra("password") ?: "1234"
+        val intent = (view.context as AppCompatActivity).intent
+        val username = intent.getStringExtra("username") ?: "Lara"
+        val password = intent.getStringExtra("password") ?: "1234"
 
-         var loginButton: Button = view.findViewById(R.id.loginButton)
-         loginButton.setOnClickListener {
-             var inputName: String= view.findViewById<EditText>(R.id.loginUsername).text.toString()
-             var inputPas: String = view.findViewById<EditText>(R.id.loginPassword).text.toString()
-             if (username == inputName && password == inputPas) {
+        var account = accountService.getById(1)
+        account.balance = intent.getDoubleExtra("balance", 100.0)
 
-                 Toast.makeText(view.context, "logged in", Toast.LENGTH_SHORT).show()
+        var loginButton: Button = view.findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            var inputName: String = view.findViewById<EditText>(R.id.loginUsername).text.toString()
+            var inputPas: String = view.findViewById<EditText>(R.id.loginPassword).text.toString()
+            if (username == inputName && password == inputPas) {
 
-                 val bundle = Bundle()
-                 bundle.putString("name", inputName)
-                 findNavController().navigate(R.id.action_loginFragment_to_userMenuFragment, bundle)
+                Toast.makeText(view.context, "logged in", Toast.LENGTH_SHORT).show()
 
-             }
-             else  Toast.makeText(view.context, "invalid credentials", Toast.LENGTH_SHORT).show()
-         }
-     }
+                val bundle = Bundle()
+                bundle.putString("name", inputName)
+                findNavController().navigate(R.id.action_loginFragment_to_userMenuFragment, bundle)
+
+            } else Toast.makeText(view.context, "invalid credentials", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
